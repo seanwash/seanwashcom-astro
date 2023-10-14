61559f05af6d4c47021573c7e02e3f5741a10dca
Sean Washington <hello@seanwash.com>
2023-10-14 08:57:36 -0700

    Add umami tracker

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+	readonly TRACKER_SRC: string;
+	readonly TRACKER_ID: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
